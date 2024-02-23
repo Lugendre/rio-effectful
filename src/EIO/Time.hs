5b91b82d3bd59eb6870f2a08cfb836364ad51a19
Lugendre <lugendre1904@gmail.com>
2024-02-24 01:20:41 +0900

    chore: add comments

--- src/EIO/Time.hs
+++ src/EIO/Time.hs
@@ -28,24 +28,32 @@ import RIO.Time hiding (
  )
 import RIO.Time qualified as T
 
+-- | An effect for interacting with the system clock.
 data Time :: Effect
+
 type instance DispatchOf Time = Static WithSideEffects
 data instance StaticRep Time = Time
 
+-- | Run a 'Time' effect by using the system clock.
 runTime :: (IOE :> es) => Eff (Time : es) a -> Eff es a
 runTime = evalStaticRep Time
 
+-- | Lifted 'T.getCurrentTime'.
 getCurrentTime :: (Time :> es) => Eff es UTCTime
 getCurrentTime = unsafeEff_ T.getCurrentTime
 
+-- | Lifted 'T.getTimeZone'.
 getTimeZone :: (Time :> es) => UTCTime -> Eff es TimeZone
 getTimeZone = unsafeEff_ . T.getTimeZone
 
+-- | Lifted 'T.getCurrentTimeZone'.
 getCurrentTimeZone :: (Time :> es) => Eff es TimeZone
 getCurrentTimeZone = unsafeEff_ T.getCurrentTimeZone
 
+-- | Lifted 'T.getZonedTime'.
 getZonedTime :: (Time :> es) => Eff es ZonedTime
 getZonedTime = unsafeEff_ T.getZonedTime
 
+-- | Lifted 'T.utcToLocalZonedTime'.
 utcToLocalZonedTime :: (Time :> es) => UTCTime -> Eff es ZonedTime
 utcToLocalZonedTime = unsafeEff_ . T.utcToLocalZonedTime
