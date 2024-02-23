b22c36d26ee70e20ad7e6068a16a800622053887
Lugendre <lugendre1904@gmail.com>
2024-02-24 02:56:25 +0900

    feat: add URef functions

--- src/EIO/Prelude/URef.hs
+++ src/EIO/Prelude/URef.hs
@@ -7,5 +7,26 @@ module EIO.Prelude.URef (
   modifyURef,
 ) where
 
--- TODO: Change to Ref Effect
-import RIO
+import Effectful
+import Effectful.Prim
+import RIO hiding (IOURef, modifyURef, newURef, readURef, writeURef)
+import RIO qualified
+
+-- | Helpful type synonym for using a URef from an Eff-based stack.
+type IOURef = URef PrimStateEff
+
+-- | Lifted 'RIO.newURef'.
+newURef :: (Prim :> es, Unbox a) => a -> Eff es (URef PrimStateEff a)
+newURef = RIO.newURef
+
+-- | Lifted 'RIO.readURef'.
+readURef :: (Prim :> es, Unbox a) => URef PrimStateEff a -> Eff es a
+readURef = RIO.readURef
+
+-- | Lifted 'RIO.writeURef'.
+writeURef :: (Prim :> es, Unbox a) => URef PrimStateEff a -> a -> Eff es ()
+writeURef = RIO.writeURef
+
+-- | Lifted 'RIO.modifyURef'.
+modifyURef :: (Prim :> es, Unbox a) => URef PrimStateEff a -> (a -> a) -> Eff es ()
+modifyURef = RIO.modifyURef
